62e82f6f6ac3651238d98f0a4e993c5f7890b7a2
karamshaarawy <karam.shaarawy@gmail.com>
2024-11-25 13:55:20 +0200

    declaring a swap function to swap the elements

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -1,3 +1,9 @@
+function swap(arr,i,j){
+    let temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+}
+
 function pivot(
     arr,start=0,end=arr.length+1
 ){
@@ -5,9 +11,9 @@ function pivot(
     let swapIndex=start
 
     for (let i=0;i<arr.length;i++){
-if (pivot>arr[i]{
+if (pivot>arr[i]){
     swapIndex++
     swap(arr,i,swapIndex)
-})
+}
     }
 }
