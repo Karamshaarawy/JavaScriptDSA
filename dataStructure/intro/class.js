6651b3742f5de5c503a7a2322d50cc802e7d84d4
karamshaarawy <karam.shaarawy@gmail.com>
2024-11-29 09:35:51 +0200

    inheritance

--- dataStructure/intro/class.js
+++ dataStructure/intro/class.js
@@ -1,8 +1,45 @@
-class Rectangle {
-    #height = 0;
-    #width;
-    constructor(height, width) {
-      this.#height = height;
-      this.#width = width;
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} makes a noise.`);
     }
   }
+  
+  class Dog extends Animal {
+    constructor(name) {
+      super(name); // call the super class constructor and pass in the name parameter
+    }
+  
+    speak() {
+      console.log(`${this.name} barks.`);
+    }
+  }
+  
+  const d = new Dog("Mitzie");
+  d.speak(); // Mitzie barks.
+
+
+  class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} makes a noise.`);
+    }
+  }
+  
+  class Lion extends Cat {
+    speak() {
+      super.speak();
+      console.log(`${this.name} roars.`);
+    }
+  }
+  
+  const l = new Lion("Fuzzy");
+  l.speak();
+  // Fuzzy makes a noise.
+  // Fuzzy roars.
