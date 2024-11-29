e50a8f87f36051f5b9590fd9c3c78050d557cfad
karamshaarawy <karam.shaarawy@gmail.com>
2024-11-29 08:56:11 +0200

    - class name should start with a capital letter - the class keyword creates a constant, so you can not redefine it - constructor is a special method used to instantiate new instances

---
+++ dataStructure/intro/class.js
@@ -0,0 +1,14 @@
+class Student{
+    constructor(firstName,lastName,year,grade){
+        this.firstName=firstName
+        this.lastName=lastName
+        this.year=year
+        this.grade=grade
+
+    }
+}
+
+//creating objects from classes
+
+let student1=new Student("student","name","year","grade")
+let student2=new Student("student2","name2","year2","grade2")
