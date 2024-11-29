774c9d3215d6d105771e92a83ce6ad87ba55a507
karamshaarawy <karam.shaarawy@gmail.com>
2024-11-29 09:16:50 +0200

    Methods

--- dataStructure/intro/class.js
+++ dataStructure/intro/class.js
@@ -3,4 +3,23 @@ class Rectangle {
       this.height = height;
       this.width = width;
     }
+    // Getter
+    get area() {
+      return this.calcArea();
+    }
+    // Method
+    calcArea() {
+      return this.height * this.width;
+    }
+    *getSides() {
+      yield this.height;
+      yield this.width;
+      yield this.height;
+      yield this.width;
+    }
   }
+  
+  const square = new Rectangle(10, 10);
+  
+  console.log(square.area); // 100
+  console.log([...square.getSides()]); // [10, 10, 10, 10]
