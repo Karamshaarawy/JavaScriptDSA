d67e2d9fb65202230ddda5b8f601316ca5ce54d0
karamshaarawy <karam.shaarawy@gmail.com>
2024-11-29 09:29:39 +0200

    Private properties

--- dataStructure/intro/class.js
+++ dataStructure/intro/class.js
@@ -1,43 +1,8 @@
-class Point {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-    }
-  
-    static displayName = "Point";
-    static distance(a, b) {
-      const dx = a.x - b.x;
-      const dy = a.y - b.y;
-  
-      return Math.hypot(dx, dy);
-    }
-  }
-  
-  const p1 = new Point(5, 5);
-  const p2 = new Point(10, 10);
-  p1.displayName; // undefined
-  p1.distance; // undefined
-  p2.displayName; // undefined
-  p2.distance; // undefined
-  
-  console.log(Point.displayName); // "Point"
-  console.log(Point.distance(p1, p2)); // 7.0710678118654755
-  
-
-
-//   class Rectangle {
-//     constructor(height, width) {
-//       this.height = height;
-//       this.width = width;
-//     }
-//   }
-
-
-  class Rectangle {
-    height = 0;
-    width;
+class Rectangle {
+    #height = 0;
+    #width;
     constructor(height, width) {
-      this.height = height;
-      this.width = width;
+      this.#height = height;
+      this.#width = width;
     }
   }
