b45c365b3a0b93fbd6f3f2d2e04c27d764bbed8e
karamshaarawy <karam.shaarawy@gmail.com>
2024-12-04 11:11:33 +0200

    use the get method to access the node at the index - 1.

--- singlyLinkedList/singlyLinkedList.js
+++ singlyLinkedList/singlyLinkedList.js
@@ -116,6 +116,7 @@ class SinglyLinkedList{
         if (index<0||index>this.length) return undefined
         if (this.length===index-1)  return this.pop(index)
         if (index===0) return this.shift()
+        let nodeBefore=get(index-1)
 
     }
 }
